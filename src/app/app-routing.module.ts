d51cd52b67e3debccad6f92d3517ba1127daeb44
arekop100 <arekop100@gmail.com>
2020-06-19 12:32:47 +0200

    commit some logic2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ShopAKComponent} from "./components/shop-ak/shop-ak.component";
+import {ShopItemAKComponent} from "./components/shop-item-ak/shop-item-ak.component";
+import {ShopDetailsAKComponent} from "./components/shop-details-ak/shop-details-ak.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShopAKComponent
+  },
+  {
+    path: 'Shop_item',
+    component: ShopItemAKComponent
+  },
+  {
+    path: 'Shop_detail',
+    component: ShopDetailsAKComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
